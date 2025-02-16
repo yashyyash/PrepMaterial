b825b33756b64b09d83a98cab4d18b1df322199a
Yash <yashnil532@gmail.com>
2025-02-16 23:11:04 +0530

    added day3 solution & renamed introduction to day 0

---
+++ Java/day0/hello.java
@@ -0,0 +1,8 @@
+package introduction;
+public class hello {
+    public static void main(String[] args){
+        int a = 23;
+        System.out.println(a);
+        System.out.println("Hello");
+    }
+}
